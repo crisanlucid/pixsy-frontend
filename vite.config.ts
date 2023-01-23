6c820de0d6d86905bf25c9639b61ec073686dd79
Lucian Crisan <crisan.lucid@yahoo.com>
2023-01-23 10:14:56 +0100

    fix: vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
